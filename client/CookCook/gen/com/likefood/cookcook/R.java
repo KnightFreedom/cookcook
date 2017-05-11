68db2a60f5d09dcaa116b43aeae74841712ef015
ILoveYourSis <sakula000@yeah.net>
2017-05-11 19:18:15 +0800

    add scrollview as menu

--- client/CookCook/gen/com/likefood/cookcook/R.java
+++ client/CookCook/gen/com/likefood/cookcook/R.java
@@ -24,9 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080007;
+        public static final int container=0x7f080006;
         public static final int guestBtn=0x7f080002;
         public static final int loginBtn=0x7f080000;
+        public static final int scrollView1=0x7f080005;
         public static final int searchBtn=0x7f080004;
         public static final int searchTxt=0x7f080003;
         public static final int signupBtn=0x7f080001;
@@ -41,10 +43,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int findCook=0x7f050008;
         public static final int guest=0x7f050007;
         public static final int hello_world=0x7f050002;
         public static final int login=0x7f050005;
         public static final int pswd=0x7f050004;
+        public static final int search=0x7f050009;
         public static final int signup=0x7f050006;
         public static final int usrName=0x7f050003;
     }
